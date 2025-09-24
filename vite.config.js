64de4148856f34084f9525625c891d72ff591f3c
geffred <tchapda20@gmail.com>
2025-09-24 15:18:27 +0200

    https true

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    https: true,
     proxy: {
       "/api": {
         target: "http://localhost:8080", // ton backend Spring Boot en local
